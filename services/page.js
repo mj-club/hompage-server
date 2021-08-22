0184f1fc4cec21911167250ace0fc3af45c43aea
HyunSBong <sanseng@mju.ac.kr>
2021-08-22 22:43:59 +0900

    service

---
+++ services/page.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+// 게시판별 게시물 보여주기
+exports.showBoard = async (boardName) => {};
+
+// 특정 게시판의 게시물 보여주기
+exports.showPost = async (postId) => {};
+
+// 모든 게시판에서 키워드로 검색하기
+exports.searchAll = async (keyword) => {};
+
+// 특정 게시판에서 키워드로 검색하기
+exports.searchByBoard = async (keyword) => {};
