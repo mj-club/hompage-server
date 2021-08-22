734c491eb5bbe3ad65a399e9a28f873692113e53
dj-1087 <sam1087@mju.ac.kr>
2021-08-23 03:01:47 +0900

    complete authService, show function

--- controller/auth.js
+++ controller/auth.js
@@ -1,7 +1,39 @@
-const { UserService } = require("../services");
+const { AuthService } = require("../services");
 
 // 회원가입
-module.exports.join = () => {};
+module.exports.join = (res, req, next) => {
+	try {
+		const user = AuthService.join(req.body);
+		res.json(user);
+	} catch (err) {
+		next(err);
+	}
+};
 
 // 로그인
-module.exports.login = () => {};
+module.exports.login = (req, res, next) => {
+	passport.authenticate("local", (authError, user, info) => {
+		if (authError) {
+			console.error(authError);
+			return next(authError);
+		}
+		if (!user) {
+			return res.json(info.message);
+		}
+		return req.login(user, (loginError) => {
+			if (loginError) {
+				console.error(loginError);
+				return next(loginError);
+			}
+
+			return res.json(user);
+		});
+	})(req, res, next);
+};
+
+// 로그아웃
+module.exports.logout = (req, res) => {
+	req.logout();
+	req.session.destroy();
+	res.json({ status: "logout" });
+};
--- services/page.js
+++ services/page.js
@@ -1,10 +1,56 @@
-const { Board, Post } = require("../models");
+const { Board, Post, Club, Union } = require("../models");
+const { NoSuchDataError } = require("../utils/handleError");
 
 // 게시판별 게시물 보여주기
-module.exports.showBoard = () => {};
+module.exports.showBoard = async (belongName, boardName) => {
+	let belong, board, posts;
+	if (belongName !== "union") {
+		belong = await Club.findOne({ where: { name: belong } });
+	} else {
+		belong = await Union.findByPk(1);
+	}
+	if (!belong) {
+		const err = NoSuchDataError("이름에 해당하는 동아리를 찾을 수 없습니다.");
+		throw err;
+	}
+	board = await belong.getBoards({
+		where: {
+			name: boardName,
+		},
+	});
+	if (!board) {
+		const err = NoSuchDataError("이름에 해당하는 게시판을 찾을 수 없습니다.");
+		throw err;
+	}
+	posts = await board[0].getPosts({
+		attributes: [
+			"id",
+			"title",
+			"thumbnail",
+			"set_top",
+			"visit_count",
+			"comment_count",
+			"thumb_count",
+		],
+		order: [["created_at", "DESC"]],
+	});
+	return posts;
+};
 
-// 특정 게시판의 게시물 보여주기
-module.exports.showPost = () => {};
+// 특정 게시물 보여주기
+module.exports.showPost = async (postId) => {
+	const post = Post.findByPk(postId, {
+		include: [Comment, File, { model: User, attributes: ["name"] }],
+	});
+	if (!post) {
+		const err = NoSuchDataError("해당 게시물을 찾을 수 없습니다.");
+		throw err;
+	}
+
+	let visit_count = parseInt(post.visit_count) + 1;
+	post = await post.update({ visit_count });
+	return post;
+};
 
 // 모든 게시판에서 키워드로 검색하기
 module.exports.searchAll = () => {};
