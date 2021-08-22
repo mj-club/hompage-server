c38e7543c352641ea5ef4c314478188e287928c5
HyunSBong <sanseng@mju.ac.kr>
2021-08-22 23:07:34 +0900

    service

--- services/page.js
+++ services/page.js
@@ -1,13 +1,13 @@
-const express = require("express");
+const { Board, Post } = require("../models");
 
 // 게시판별 게시물 보여주기
-exports.showBoard = async (boardName) => {};
+module.exports.showBoard = () => {};
 
 // 특정 게시판의 게시물 보여주기
-exports.showPost = async (postId) => {};
+module.exports.showPost = () => {};
 
 // 모든 게시판에서 키워드로 검색하기
-exports.searchAll = async (keyword) => {};
+module.exports.searchAll = () => {};
 
 // 특정 게시판에서 키워드로 검색하기
-exports.searchByBoard = async (keyword) => {};
+module.exports.searchByBoard = () => {};
--- utils/permission.js
+++ utils/permission.js
@@ -1,13 +1,13 @@
-const express = require("express");
+const { Manager } = require("../models");
 
 // 권한 확인 - 일반 학생인지, 동아리인지, 총동연인지
-exports.isManager = async () => {};
+module.exports.isManager = () => {};
 
 // 동아리 권한 확인  
-exports.isClubManager = async () => {};
+module.exports.isClubManager = () => {};
 
 // 총동연 권한 확인  
-exports.isUnionManager = async () => {};
+module.exports.isUnionManager = () => {};
 
 // 계정 타입 확인 - 일반 학생 계정인지, 동아리 계정인지, 총동연 계정인지
-exports.checkAccountType = async () => {};
+module.exports.checkAccountType = () => {};
