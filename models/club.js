d18204e8c797bc6388511e533edd17342d2164f0
dj-1087 <sam1087@mju.ac.kr>
2021-08-25 22:48:25 +0900

    update schedule assotiaction

--- models/club.js
+++ models/club.js
@@ -1,48 +1,54 @@
 const Sequelize = require("sequelize");
 
 module.exports = class Club extends Sequelize.Model {
-  static init(sequelize) {
-    return super.init(
-      {
-        name: {
-          // 동아리 명
-          type: Sequelize.STRING(45),
-          allowNull: false,
-        },
-      },
-      {
-        sequelize,
-        modelName: "Club",
-        tableName: "clubs",
-        timestamps: true,
-        underscored: true,
-        paranoid: false,
-        charset: "utf8mb4",
-        collate: "utf8mb4_unicode_ci",
-      }
-    );
-  }
+	static init(sequelize) {
+		return super.init(
+			{
+				name: {
+					// 동아리 명
+					type: Sequelize.STRING(45),
+					allowNull: false,
+				},
+			},
+			{
+				sequelize,
+				modelName: "Club",
+				tableName: "clubs",
+				timestamps: true,
+				underscored: true,
+				paranoid: false,
+				charset: "utf8mb4",
+				collate: "utf8mb4_unicode_ci",
+			}
+		);
+	}
 
-  static associate(db) {
-    // Club - ClubInfo (1:1)
-    db.Club.hasOne(db.ClubInfo, {
-      foreignKey: "club_id",
-      sourceKey: "id",
-    });
+	static associate(db) {
+		// Club - ClubInfo (1:1)
+		db.Club.hasOne(db.ClubInfo, {
+			foreignKey: "club_id",
+			sourceKey: "id",
+		});
 
-    // Club - User (n:m)
-    db.Club.belongsToMany(db.User, { through: db.Member });
+		// Club - User (n:m)
+		db.Club.belongsToMany(db.User, { through: db.Member });
 
-    // Club - Manager (1:1)
-    db.Club.hasOne(db.Manager, {
-      foreignKey: "club_id",
-      sourceKey: "id",
-    });
+		// Club - Manager (1:1)
+		db.Club.hasOne(db.Manager, {
+			foreignKey: "club_id",
+			sourceKey: "id",
+		});
 
-    // Club - Board (1:n)
-    db.Club.hasMany(db.Board, {
-      foreignKey: "club_id",
-      sourceKey: "id",
-    });
-  }
+		// Club - Board (1:n)
+		db.Club.hasMany(db.Board, {
+			foreignKey: "club_id",
+			sourceKey: "id",
+		});
+
+		// Club - Schedule (1:n)
+		db.Club.hasMany(db.Schedule, {
+			foreignKey: "club_id",
+			sourceKey: "id",
+		});
+	}
 };
--- models/union.js
+++ models/union.js
@@ -1,46 +1,55 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
 module.exports = class Union extends Sequelize.Model {
-  static init(sequelize) {
-    return super.init(
-      {
-        name: { // 총동아리연합회(default)
-          type: Sequelize.STRING(45),
-          allowNull: false
-        },
-        introduction: { // 총동아리연합회 소개(default)
-          type: Sequelize.TEXT,
-          allowNull: true
-        },
-      }, {
-        sequelize,
-        modelName: "Union",
-        tableName: 'union',
-        timestamps: true,
-        underscored: true,
-        paranoid: false,
-        charset: "utf8mb4",
-        collate: "utf8mb4_unicode_ci"
-      }
-    );
-  }
-  static associate(db) {
-    // Union - UnionInfo (1:n)
-    db.Union.hasMany(db.UnionInfo, {
-      foreignKey: "union_id",
-      sourceKey: "id",
-    });
+	static init(sequelize) {
+		return super.init(
+			{
+				name: {
+					// 총동아리연합회(default)
+					type: Sequelize.STRING(45),
+					allowNull: false,
+				},
+				introduction: {
+					// 총동아리연합회 소개(default)
+					type: Sequelize.TEXT,
+					allowNull: true,
+				},
+			},
+			{
+				sequelize,
+				modelName: "Union",
+				tableName: "union",
+				timestamps: true,
+				underscored: true,
+				paranoid: false,
+				charset: "utf8mb4",
+				collate: "utf8mb4_unicode_ci",
+			}
+		);
+	}
+	static associate(db) {
+		// Union - UnionInfo (1:n)
+		db.Union.hasMany(db.UnionInfo, {
+			foreignKey: "union_id",
+			sourceKey: "id",
+		});
 
-    // Union - Board (1:n)
-    db.Union.hasMany(db.Board, {
-      foreignKey: "union_id",
-      sourceKey: "id",
-    });
+		// Union - Board (1:n)
+		db.Union.hasMany(db.Board, {
+			foreignKey: "union_id",
+			sourceKey: "id",
+		});
 
-    // Union - Manager (1:1)
-    db.Union.hasOne(db.Manager, {
-      foreignKey: "union_id",
-      sourceKey: "id",
-    });
-  }
+		// Union - Manager (1:1)
+		db.Union.hasOne(db.Manager, {
+			foreignKey: "union_id",
+			sourceKey: "id",
+		});
+
+		// Union - Schedule (1:n)
+		db.Union.hasMany(db.Schedule, {
+			foreignKey: "union_id",
+			sourceKey: "id",
+		});
+	}
 };
