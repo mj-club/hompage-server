bf3d746f11d82737d25b143966d2e1105011034e
dj-1087 <sam1087@mju.ac.kr>
2021-08-31 18:59:08 +0900

    for pull

--- controller/post.js
+++ controller/post.js
@@ -1,13 +1,39 @@
-const {} = require("../services");
+const { UserService, PageService } = require("../services");
 
 // 게시물 등록
-module.exports.addPost = (res, req, next) => {};
+module.exports.addPost = (res, req, next) => {
+	try {
+	} catch (err) {
+		next(err);
+	}
+};
 
 // 게시물 보여주기
-module.exports.showPost = (res, req, next) => {};
+module.exports.showPost = (res, req, next) => {
+	try {
+		const post = PageService.showPost(req.params.postId);
+		return post;
+	} catch (err) {
+		next(err);
+	}
+};
 
 // 게시물 수정하기
-module.exports.editPost = (res, req, next) => {};
+module.exports.editPost = (res, req, next) => {
+	try {
+		const post = UserService.editPost(req.params.postId, req.body);
+		return post;
+	} catch (err) {
+		next(err);
+	}
+};
 
 // 게시물 삭제하기
-module.exports.removePost = (res, req, next) => {};
+module.exports.removePost = (res, req, next) => {
+	try {
+		const post = UserService.removePost(req.params.postId);
+		return post;
+	} catch (err) {
+		next(err);
+	}
+};
--- utils/permission.js
+++ utils/permission.js
@@ -3,10 +3,10 @@ const { Manager } = require("../models");
 // 권한 확인 - 일반 학생인지, 동아리인지, 총동연인지
 module.exports.isManager = () => {};
 
-// 동아리 권한 확인  
+// 동아리 권한 확인
 module.exports.isClubManager = () => {};
 
-// 총동연 권한 확인  
+// 총동연 권한 확인
 module.exports.isUnionManager = () => {};
 
 // 계정 타입 확인 - 일반 학생 계정인지, 동아리 계정인지, 총동연 계정인지
