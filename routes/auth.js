162015c7cb31eeccbe87d0943fafeae47a4e0aaa
dj-1087 <sam1087@mju.ac.kr>
2021-09-06 22:40:58 +0900

    add multer

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,10 @@
 var express = require("express");
+const multer = require("multer");
 var router = express.Router();
 var AuthController = require("../controller/auth");
 
 /* 회원가입 */
-router.post("/join", AuthController.join);
+router.post("/join", multer().none(), AuthController.join);
 
 /* 로그인 */
 router.post("/login", AuthController.login);
@@ -12,27 +13,31 @@ router.post("/login", AuthController.login);
 router.get("/logout", AuthController.logout);
 
 /* 이메일 찾기 */
-router.post("/findEmail", AuthController.findEmail);
+router.post("/findEmail", multer().none(), AuthController.findEmail);
 
 /* 토큰 요청 */
-router.post("/sendTokenToMail", AuthController.sendTokenToMail);
+router.post(
+	"/sendTokenToMail",
+	multer().none(),
+	AuthController.sendTokenToMail
+);
 
 /* 비번 재설정 */
-router.post("/resetPW/:token", AuthController.resetPW);
+router.post("/resetPW/:token", multer().none(), AuthController.resetPW);
 
 /* 회원 탈퇴 */
-router.post("/leave", AuthController.leave);
+router.post("/leave", multer().none(), AuthController.leave);
 
 /* 계정 권한 확인하기 */
 router.get("/checkPermission/:accountType", AuthController.leave);
 
 /* 이메일 중복확인 */
-router.post("/checkEmail", AuthController.checkEmail);
+router.post("/checkEmail", multer().none(), AuthController.checkEmail);
 
 /* 학번 중복확인 */
-router.post("/checkStudentId", AuthController.checkStudentId);
+router.post("/checkStudentId", multer().none(), AuthController.checkStudentId);
 
 /* 휴대폰번호 중복확인 */
-router.post("/ccheckPh", AuthController.checkPh);
+router.post("/ccheckPh", multer().none(), AuthController.checkPh);
 
 module.exports = router;
--- routes/comment.js
+++ routes/comment.js
@@ -3,11 +3,12 @@ var router = express.Router();
 var CommentController = require("../controller/comment");
 
 /* 댓글 등록 */
-router.post("/:postId", CommentController.addComment);
+router.post("/:postId", multer().none(), CommentController.addComment);
 
 /* 댓글 수정, 삭제 */
-router.route("/:commentId")
-  .patch(CommentController.editComment)
-  .delete(CommentController.removeComment)
+router
+	.route("/:commentId")
+	.patch(multer().none(), CommentController.editComment)
+	.delete(CommentController.removeComment);
 
 module.exports = router;
